e272209611e5b76e20762c39bb21572b508c7e4a
Anssi Herranen <ext-anssi.herranen@barona.fi>
2022-05-31 12:48:47 +0200

    Add eslint

--- lib/clammy-stack.ts
+++ lib/clammy-stack.ts
@@ -1,45 +1,22 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { join } from 'path';
-
-// https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda_nodejs.NodejsFunctionProps.html
-const propsss: NodejsFunctionProps = {
-  runtime: Runtime.NODEJS_16_X,
-  // entry: join(__dirname, 'lambdas', 'my-lambda.ts'),
-  bundling: {
-    externalModules: [
-      'aws-sdk', // Use the 'aws-sdk' available in the Lambda runtime
-      // 'cool-module', // 'cool-module' is already available in a Layer
-    ],
-  },
-}
-
-const props: NodejsFunctionProps = {
-  
-    memorySize: 1024,
-    timeout: cdk.Duration.seconds(5),
-    runtime: Runtime.NODEJS_16_X,
-    handler: 'main',
-    entry: join(__dirname, 'lambdas', 'my-lambda.ts'),
-    // entry: join(__dirname, '/../src/my-lambda/index.ts'),
-  
-}
-
-// lib/clammy-stack.myNodeLambda.mjs lib/lambdas/my-lambda.ts
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import { Runtime } from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { join } from 'path'
 
 export class ClammyStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor (scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
 
+    // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda_nodejs.NodejsFunctionProps.html
+    // eslint-disable-next-line no-unused-vars
     const myFunction = new NodejsFunction(this, 'my-function', {
       memorySize: 1024,
       timeout: cdk.Duration.seconds(5),
       runtime: Runtime.NODEJS_16_X,
       // handler: 'main',
       entry: join(__dirname, 'lambdas', 'my-lambda.ts'),
-    });
+    })
 
     // const myLambda = new NodejsFunction(this, '/lambdas/my-lambda', props)
 
